ad549e93de747761db692e5a7c2aef6721516f1d
GrimmAuxiliatrix <ujala601@gmail.com>
2017-12-03 16:08:31 -0500

    Add files via upload

---
+++ GemSpawn.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GemSpawn : MonoBehaviour {
+
+	public GameObject[] gems = new GameObject[2];
+	public int count;
+	public float vVal = 2.5f;
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < count; i++) {
+			foreach (GameObject gem in gems){
+				Instantiate (gem, new Vector3(Random.Range (-10, 10), vVal, 0), Quaternion.identity);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ OrcController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OrcController : MonoBehaviour {
+
+	public Animator anim;
+	public float spd = 10f;
+	public Vector2 howHigh;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey ("d")) {
+			anim.SetInteger ("state", 1); 
+			var move = new Vector3 (Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical"), 0);
+			transform.position += move * spd * Time.deltaTime;
+		} else if (Input.GetKey ("a")) {
+			anim.SetInteger ("state", 1); 
+			var move = new Vector3 (Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical"), 0);
+			transform.position += move * spd * Time.deltaTime;
+		} else {
+			anim.SetInteger ("state", 0);
+		}
+		
+		if(Input.GetKeyDown("w")){
+			GetComponent<Rigidbody2D>().AddForce(howHigh, ForceMode2D.Impulse);
+		}
+	}
+	
+	void OnTriggerEnter(Collider other){
+		if(other.CompareTag("collectible")){
+			Destroy(other.gameObject);
+		}
+	}
+}
