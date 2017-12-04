1575f18b142be8ffb73971097fef2e02d2b51a04
alawngnome <32248903+alawngnome@users.noreply.github.com>
2017-12-03 19:24:24 -0500

    For the main menu (probably works)

---
+++ Menu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+	public Canvas MainCanvas;
+	public Canvas HowToCanvas;
+
+	void Awake() {
+		HowToCanvas.enabled = false;
+	}
+
+	public void HowToOn() {
+		HowToCanvas.enabled = true;
+		MainCanvas.enabled = false;
+	}
+
+	public void ReturnOn() {
+		HowToCanvas.enabled = false;
+		MainCanvas.enabled = true;
+	}
+	public void LoadOn() {
+		SceneManager.LoadScene("scene", LoadSceneMode.Additive);
+	}
+}
