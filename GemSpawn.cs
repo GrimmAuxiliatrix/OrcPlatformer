2cfce02ea96a127184e5a2c9aa0bcada0269a802
GrimmAuxiliatrix <ujala601@gmail.com>
2017-12-03 19:42:47 -0500

    Also spawns platforms and the orc!

--- GemSpawn.cs
+++ GemSpawn.cs
@@ -1,17 +1,25 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class GemSpawn : MonoBehaviour {
 
 	public GameObject[] gems = new GameObject[2];
-	public int count;
-	public float vVal = 2.5f;
+	public float vVal = 3f;
+	public GameObject platform;
+	public GameObject orc;
+	public float[] genValues = new float[]{-9f, -6f, -1f, 1f, 6f, 9f}; //x-value ranges for platforms
+	public float yMin = -1f;
+	public float yMax = 3f;
 
 	// Use this for initialization
 	void Start () {
-		for (int i = 0; i < count; i++) {
+		for (int i = 0; i < 6; i+=2) {
+			Instantiate(platform, new Vector3(Random.Range(genValues[i], genValues[i+1]), Random.Range(yMin, yMax), -1), Quaternion.identity);
 			foreach (GameObject gem in gems){
-				Instantiate (gem, new Vector3(Random.Range (-10, 10), vVal, -1), Quaternion.identity);
+				Instantiate (gem, new Vector3(Random.Range (genValues[i], genValues[i+1]), vVal, -1), Quaternion.identity);
+			}
+			if(i==0){
+				Instantiate(orc, new Vector3(-8, vVal, -1), Quaternion.identity);
 			}
 		}
 	}
