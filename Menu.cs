1b360845c5d4f4a623aebd016c3729d1584052b0
alawngnome <32248903+alawngnome@users.noreply.github.com>
2017-12-04 20:34:18 -0500

    for win screen

--- Menu.cs
+++ Menu.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,9 +7,17 @@ using UnityEngine.SceneManagement;
 public class Menu : MonoBehaviour {
 	public Canvas MainCanvas;
 	public Canvas HowToCanvas;
+	public Canvas WinCanvas;
 
 	void Awake() {
 		HowToCanvas.enabled = false;
+		WinCanvas.enabled = false;
+
+		if (SceneManager.sceneCount >= 2) {
+			SceneManager.UnloadSceneAsync ("scene");
+			MainCanvas.enabled = false;
+			WinCanvas.enabled = true;
+		}
 	}
 
 	public void HowToOn() {
@@ -22,6 +30,12 @@ public class Menu : MonoBehaviour {
 		MainCanvas.enabled = true;
 	}
 	public void LoadOn() {
-		SceneManager.LoadScene("scene", LoadSceneMode.Additive);
+		SceneManager.LoadScene("scene", LoadSceneMode.Single);
+		//MainCanvas.enabled = false;
 	}
+
+	public void QuitOn(){
+		Application.Quit();
+	}
+
 }
